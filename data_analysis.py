4f82812fd566c1a7db93c1f49cddaaf098feb752
Mysxtic <dayvionpeoples4@gmail.com>
2026-01-15 19:16:12 +0000

    very basic data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,24 @@
 # Dayvion Peoples
+
+import pandas as pd
+
+# Read the csv file into a variable
+df = pd.read_csv("popular_anime.csv")
+
+# Dispaly first few rows in the dataset
+print(df.head(10))
+
+
+# Put the id cloumn into a variable
+id_count = df["id"]
+
+# Display largest id count
+print(id_count.max())
+
+largest_id_count = df[df['id'] == df['id'].max()]
+print("Largest number of id")
+print()
+print(largest_id_count)
+
+# Display smallest id count
+#print(id_count.min())
