9139bd188621b9667115bfc55b71661675fb6986
Mysxtic <dayvionpeoples4@gmail.com>
2026-01-15 19:32:35 +0000

    made a chart

--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,14 @@
 # Dayvion Peoples
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # Read the csv file into a variable
 df = pd.read_csv("popular_anime.csv")
 
 # Dispaly first few rows in the dataset
 print(df.head(10))
-
 
 # Put the id cloumn into a variable
 id_count = df["id"]
@@ -22,3 +23,13 @@ print(largest_id_count)
 
 # Display smallest id count
 #print(id_count.min())
+
+
+# Plot name and status
+
+plt.plot(df['name'], df['status'])
+plt.xlabel("Name")
+plt.ylabel("Status")
+plt.title("Name and status of animes")
+plt.savefig("output.png")
+plt.show()
